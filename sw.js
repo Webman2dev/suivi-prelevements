a0a2ec7ae6e603c18ba7ebc9733f6f3c0d5fd7de
Webman2dev <webman2@club.fr>
2025-11-28 09:19:54 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,73 @@
+const CACHE_NAME = 'prelevements-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './android-chrome-192x192.png',
+  './android-chrome-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/lucide/0.263.1/umd/lucide.min.js'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activation du Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression ancien cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Retourne depuis le cache si disponible
+        if (response) {
+          return response;
+        }
+        
+        // Sinon, fait la requête réseau
+        return fetch(event.request).then((response) => {
+          // Ne met en cache que les requêtes GET valides
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          
+          // Clone la réponse
+          const responseToCache = response.clone();
+          
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+          
+          return response;
+        });
+      })
+      .catch(() => {
+        // En cas d'erreur, retourne le cache si disponible
+        return caches.match('./index.html');
+      })
+  );
+});
